adb6a880f9585475445ae2803ae24ca62a1835c3
trinhtc <congtrinh097@gmail.com>
2023-03-10 13:29:49 +0700

    Update ContentScript

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,46 @@
 console.log('This is the background page.');
-console.log('Put the background scripts here.');
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log(details);
+ 
+  chrome.contextMenus.create({
+    title: "English for Beginners",
+    id: "contextMenuMain",
+    contexts: [ "page", "selection"]
+  })
+
+  chrome.contextMenus.create({
+    title: "Translate Selected",
+    id: "translateSelectedText",
+    parentId: "contextMenuMain",
+    contexts: ["selection"]
+  })
+
+  chrome.contextMenus.create({
+    title: "Add to Learning List",
+    id: "addToLearningList",
+    parentId: "contextMenuMain",
+    contexts: [ "selection"]
+  })
+
+  chrome.contextMenus.create({
+    title: "Read Selected Text",
+    id: "readSelectedText",
+    parentId: "contextMenuMain",
+    contexts: [ "selection"]
+  })
+
+});
+
+chrome.contextMenus.onClicked.addListener((event) => { 
+  console.log(event);
+  if(event.menuItemId === 'translateSelectedText') {
+    chrome.tabs.create({
+      url: `http://translate.google.com.vn/?hl=vi&sl=en&tl=vi&text=${event.selectionText}&op=translate`})
+  }
+  if(event.menuItemId === 'readSelectedText') {
+    chrome.tts.speak(event.selectionText, {'lang': 'en-US', 'rate': 1.5});
+  }
+  if(event.menuItemId === 'addToLearningList') {
+   // Add to learning list
+  }
+});
--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -5,8 +5,21 @@ interface Props {
   title: string;
 }
 
+const save = () => {
+  console.log('save');
+}
+
 const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+
+
+  return (<div className="OptionsContainer">
+    <h2>
+    {title} Options
+    </h2>
+    <div className='contaner-options'>
+      <button className="btn-save" onClick={save}>Save</button>
+    </div>
+  </div>);
 };
 
 export default Options;
