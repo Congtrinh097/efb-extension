885a81e81c13a6a3109b9eba46b3ab2bc032a6aa
trinhtc <congtrinh097@gmail.com>
2023-03-14 16:55:43 +0700

    add UI framework

--- src/pages/WordList/WordList.tsx
+++ src/pages/WordList/WordList.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './WordList.css';
+import EnhancedTable from './WordTable';
 
 const WordList: React.FC = () => {
   return (
     <div className="container">
       <h1>Word List Page</h1>
+      <EnhancedTable></EnhancedTable>
     </div>
   );
 };
